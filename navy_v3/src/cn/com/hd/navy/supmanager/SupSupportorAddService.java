7b39bb3a08ddde01220681763dd7996804a936e4
mc <mc@47ee70cb-904c-3e43-b3d1-3900189539e7>
2012-08-18 07:44:22 +0000

    git-svn-id: https://zuow-pc:8443/svn/navy@106 47ee70cb-904c-3e43-b3d1-3900189539e7

--- navy_v3/src/cn/com/hd/navy/supmanager/SupSupportorAddService.java
+++ navy_v3/src/cn/com/hd/navy/supmanager/SupSupportorAddService.java
@@ -6,25 +6,27 @@ import cn.com.hd.dto.navy.THighWay;
 import cn.com.hd.dto.navy.TSupTrans;
 import cn.com.hd.dto.navy.TSupportor;
 import cn.com.hd.dto.navy.TTransport;
+import cn.com.hd.error.Debug;
 import cn.com.hd.service.BaseService;
 import cn.com.hd.service.IService;
 import cn.com.hd.transfer.Conditions;
 import cn.com.hd.transfer.Request;
 import cn.com.hd.transfer.Response;
 
+public class SupSupportorAddService extends BaseService implements IService {
 
-public class SupSupportorUpdateService extends BaseService implements IService {
-
-	@SuppressWarnings("unchecked")
 	public Response service(Request request) throws Exception {
 		Response response = new Response();
 		
+		//获取数据
 		TSupportor supportor = new TSupportor();
 		super.getData(request.getDto(), supportor);
-
+		
+		Debug.debugMessage(1, request, supportor.toString());
+		
 		Conditions cons = new Conditions();
 		cons.addCondition(new TSupportor());
-		cons.addExpression("SUP_NAME = '" + supportor.getSupname() + "' AND SUP_ID != '" + supportor.getSupid() + "'");
+		cons.addExpression("SUP_NAME = '" + supportor.getSupname() + "' OR SUP_EN_NAME = '" + supportor.getSupenname() + "'");
 		
 		if (queryResultSet(cons).getRowCount() > 0) {
 			Request res = new Request();
@@ -44,7 +46,7 @@ public class SupSupportorUpdateService extends BaseService implements IService {
 		
 		cons = new Conditions();
 		cons.addCondition(new TSupportor());
-		cons.addExpression("ORGANIZE_CODE = '" + supportor.getOrganizecode() + "' AND SUP_ID != '" + supportor.getSupid() + "'");
+		cons.addExpression("ORGANIZE_CODE = '" + supportor.getOrganizecode() + "'");
 		
 		if (queryResultSet(cons).getRowCount() > 0) {
 			Request res = new Request();
@@ -63,37 +65,39 @@ public class SupSupportorUpdateService extends BaseService implements IService {
 		}
 		
 		if (supportor.getSupid() == null) {
-			response.setRequestParam(request.getDto());
-			response.setResult(0);
-			response.getDto().setList("RESULT", getDTO(supportor));
-			response.setErrorInfo("供应商ID为空，修改失败");
-			return response;
+			supportor.setSupid(UUID.randomUUID().toString());
 		}
 		
-		int result = super.update(supportor);
+		//保存供应商数据
+		int result = super.save(supportor);
 		
-		String sql = "UPDATE T_IMPORT IM SET SUPPORTOR = '" + supportor.getSupname() + 
-				"', SUPPORTOR_ADDR = '" + supportor.getAddress() + 
-				"' WHERE SUPPORTOR_ID = '" + supportor.getSupid() + "'";
-		
-		result = executeSql(sql, null, null);
-		
-		/////////////////////////////////////////////////////////////////////////////////
-		// 更新运输信息
+		//////////////////////////////////////////////////////////////////////////////////////////////////
+		// 保存运输相关信息
 		TSupTrans supTrans = new TSupTrans();
 		super.getData(request.getDto(), supTrans);
-		if(supTrans.getComid()==null || supTrans.getComid().equals("")) {
-			supTrans.setComid(UUID.randomUUID().toString());
-			super.save(supTrans);		
-		} else {
-			super.update(supTrans);
+		if(supTrans.getComid()!=null) {
+			Request res = new Request();
+			res.setResponseSystemName("HDDict");
+			res.setResponseSubsystemName("DictManage");
+			res.setResponseServiceName("SupportorDictQueryService");
+			Response resp = requestService(res);
+			
+			response.getDto().setSelectItems(resp.getDto().getList("RESULT"));
+			response.setRequestParam(request.getDto());
+			response.getDto().setList("RESULT", getDTO(supportor));
+			response.setErrorInfo("供应商-运输服务企业对应关系已存在");
+			response.setResult(0);
+			
+			return response;
 		}
+		supTrans.setComid(UUID.randomUUID().toString());
+		super.save(supTrans);	
 		
 		TTransport trans = new TTransport();
 		super.getData(request.getDto(), trans);
 		cons = new Conditions();
 		cons.addCondition(trans);
-		cons.addExpression("COM_NAME='" + trans.getComname() + "' AND COM_ID!=" + supTrans.getComid() + "'");
+		cons.addExpression("COM_NAME='" + trans.getComname() + "' AND COM_ID!=" + trans.getComid() + "'");
 		if(queryResultSet(cons).getRowCount()>0) {
 			Request res = new Request();
 			res.setResponseSystemName("HDDict");
@@ -109,25 +113,24 @@ public class SupSupportorUpdateService extends BaseService implements IService {
 			
 			return response;			
 		}
-		super.update(trans);
+		trans.setComid(supTrans.getComid());
+		super.save(trans);
 		
 		THighWay hiw = new THighWay();
 		super.getData(request.getDto(), hiw);
 		cons = new Conditions();
 		cons.addCondition(hiw);
-		cons.addExpression("COM_ID='" + supTrans.getComid() + "' AND HIW_ID='" + hiw.getHiwid() + "'");
+		cons.addExpression("COM_ID='" + hiw.getComid() + "' AND HIW_ID='" + hiw.getHiwid() + "'");
 		if(queryResultSet(cons).getRowCount()==0) {
 			hiw.setComid(supTrans.getComid());
 			super.save(hiw);
-		} else {
-			super.update(hiw);
-		}
+		}		
 		
-		response.getDto().put("REQUEST_PARAM", request.getDto());
+		response.setRequestParam(request.getDto());
 		response.getDto().setList("RESULT", getDTO(supportor));
-		response.getDto().put("RESULT", result);
+		response.setResult(result);
 		
 		return response;
 	}
 
-}		
+}
